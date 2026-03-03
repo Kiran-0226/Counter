ede50464bebd3c5c4191f7041af101b2862ea60f
Kiran-0226 <kirangowda0226@gmail.com>
2026-03-03 22:43:47 +0530

    Updated the code

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const counter = document.getElementById('count');
 const increment = document.getElementById('increment');
+const reset = document.getElementById('reset');
 
 let count = 0;
 
 increment.addEventListener('click', () => {
     count++;
+    counter.textContent = count;
+})
+
+reset.addEventListener('click', () => {
+    count = 0;
     counter.textContent = count;
 })
