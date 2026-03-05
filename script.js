e86c9ad636f3450d0310e703c231075103c5cf8a
Kiran-0226 <kirangowda0226@gmail.com>
2026-03-05 22:00:42 +0530

    Updated version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const counter = document.getElementById('count');
 const increment = document.getElementById('increment');
 const reset = document.getElementById('reset');
+const decrement = document.getElementById('decrement');
 
 let count = 0;
 
@@ -12,4 +13,11 @@ increment.addEventListener('click', () => {
 reset.addEventListener('click', () => {
     count = 0;
     counter.textContent = count;
+})
+
+decrement.addEventListener('click', () => {
+    if (count > 0) {
+        count--;
+        counter.textContent = count;
+    }
 })
